7b2d9cb04369f9c693c55b22f70e54d17e77718c
meldofficial9 <meldofficial9@gmail.com>
2025-10-22 17:39:08 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './index.css'           // <- IMPORTANTE para que Tailwind se aplique
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
+
